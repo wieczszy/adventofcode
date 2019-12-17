6ae9d36f0818b3a43052dc97531c18a87423d93e
wieczszy <5509131+wieczszy@users.noreply.github.com>
2019-12-17 14:54:35 +0100

    move intcomputer to separate file

---
+++ aoc/computer.py
@@ -0,0 +1,75 @@
+class Computer():
+    def __init__(self, program):
+        self.state = 1
+        self.idx = 0
+        self.output_value = None
+        self.program = program
+
+    def get_output(self):
+         return self.output_value
+
+    def halt(self):
+        self.state = 0
+
+    def compute(self, input_value):
+        while self.state:
+            instruction = str(self.program[self.idx])
+
+            while len(instruction) < 4:
+                instruction = '0' + instruction 
+
+            opcode = int(instruction[-2:])
+            modes = [int(c) for c in instruction[:-2]][::-1]
+
+            if opcode == 1:
+                pos1, pos2, pos3 = self.program[self.idx+1], self.program[self.idx+2], self.program[self.idx+3]
+                self.program[pos3] = (pos1 if modes[0] == 1 else self.program[pos1]) + (pos2 if modes[1] == 1 else self.program[pos2])
+                self.idx += 4
+            elif opcode == 2:
+                pos1, pos2, pos3 = self.program[self.idx+1], self.program[self.idx+2], self.program[self.idx+3]
+                self.program[pos3] = (pos1 if modes[0] == 1 else self.program[pos1]) * (pos2 if modes[1] == 1 else self.program[pos2])
+                self.idx += 4
+            elif opcode == 3:
+                if input_value == []:
+                    break
+                pos1 = self.program[self.idx+1]
+                self.program[pos1] = input_value[0]
+                del input_value[0]
+                self.idx += 2
+            elif opcode == 4:
+                pos1 = self.program[self.idx+1]
+                self.output_value = self.program[pos1]
+                self.idx += 2
+                break
+            elif opcode == 5:
+                pos1, pos2 = self.program[self.idx+1], self.program[self.idx+2]
+                if (pos1 if modes[0] == 1 else self.program[pos1]):
+                    self.idx = (pos2 if modes[1] == 1 else self.program[pos2])
+                else:
+                    self.idx += 3
+            elif opcode == 6:
+                pos1, pos2 = self.program[self.idx+1], self.program[self.idx+2]
+                if not (pos1 if modes[0] == 1 else self.program[pos1]):
+                    self.idx = (pos2 if modes[1] == 1 else self.program[pos2])
+                else:
+                    self.idx += 3
+            elif opcode == 7:
+                pos1, pos2, pos3 = self.program[self.idx+1], self.program[self.idx+2], self.program[self.idx+3]
+                if (pos1 if modes[0] == 1 else self.program[pos1]) < (pos2 if modes[1] == 1 else self.program[pos2]):
+                    self.program[pos3] = 1
+                else:
+                    self.program[pos3] = 0
+                self.idx += 4
+            elif opcode == 8:
+                pos1, pos2, pos3 = self.program[self.idx+1], self.program[self.idx+2], self.program[self.idx+3]
+                if (pos1 if modes[0] == 1 else self.program[pos1]) == (pos2 if modes[1] == 1 else self.program[pos2]):
+                    self.program[pos3] = 1
+                else:
+                    self.program[pos3] = 0
+                self.idx += 4
+            else:
+                assert opcode == 99
+                self.state = 0
+                break
+
+        return self.get_output()
