057f75a78b7a2cc3ca7aed97fe64f50d94dbb0c0
wieczszy <5509131+wieczszy@users.noreply.github.com>
2019-12-17 14:54:50 +0100

    day 07 solved

--- aoc/day_07.py
+++ aoc/day_07.py
@@ -1,73 +1,39 @@
 from itertools import permutations
+from computer import Computer
 
-class Computer():
-    def __init__(self, program):
-        self.state = 1
-        self.idx = 0
-        self.output_value = None
-        self.program = program
+### INPUT ###
+program = [int(x) for x in open('data/day_07.csv', 'r').read().split(',')]
 
-    def get_output(self):
-         return self.state, self.idx, self.output_value
+### PART 1 ###
+settings = permutations([i for i in range(5)], 5)
+R = 0
+for s in settings:
+    c = Computer(program)
+    tmp = c.compute([s[0], 0])
+    for i in range(1,5):
+        c = Computer(program)
+        tmp = c.compute([s[i], tmp])
+    if tmp > R:
+        R = tmp
+    
+print(R)
 
-    def halt(self):
-        self.state = 0
+### PART 2 ###
+settings = permutations([i for i in range(5,10)], 5)
+R = 0
 
-    def compute(self, input_value):
-        while self.state:
-            instruction = str(self.program[self.idx])
+for s in settings:
+    computers = [Computer(program) for i in range(5)]
+    values = [0] * len(computers)
+    for i in range(len(computers)):
+        _ = computers[i].compute([s[i]])
+    while computers[-1].state:
+        values[1] = computers[0].compute([values[0]])
+        values[2] = computers[1].compute([values[1]])
+        values[3] = computers[2].compute([values[2]])
+        values[4] = computers[3].compute([values[3]])
+        values[0] = computers[4].compute([values[4]])
+    if computers[-1].get_output() > R:
+        R = computers[-1].get_output()
 
-            while len(instruction) < 4:
-                instruction = '0' + instruction 
-
-            opcode = int(instruction[-2:])
-            modes = [int(c) for c in instruction[:-2]][::-1]
-
-            if opcode == 1:
-                pos1, pos2, pos3 = self.program[self.idx+1], self.program[self.idx+2], self.program[self.idx+3]
-                self.program[pos3] = (pos1 if modes[0] == 1 else self.program[pos1]) + (pos2 if modes[1] == 1 else self.program[pos2])
-                self.idx += 4
-            elif opcode == 2:
-                pos1, pos2, pos3 = self.program[self.idx+1], self.program[self.idx+2], self.program[self.idx+3]
-                self.program[pos3] = (pos1 if modes[0] == 1 else self.program[pos1]) * (pos2 if modes[1] == 1 else self.program[pos2])
-                self.idx += 4
-            elif opcode == 3:
-                pos1 = self.program[self.idx+1]
-                self.program[pos1] = input_value
-                self.idx += 2
-            elif opcode == 4:
-                pos1 = self.program[self.idx+1]
-                self.output_value = self.program[pos1]
-                self.idx += 2
-                self.get_output()
-            elif opcode == 5:
-                pos1, pos2 = self.program[self.idx+1], self.program[self.idx+2]
-                if (pos1 if modes[0] == 1 else self.program[pos1]):
-                    self.idx = (pos2 if modes[1] == 1 else self.program[pos2])
-                else:
-                    self.idx += 3
-            elif opcode == 6:
-                pos1, pos2 = self.program[self.idx+1], self.program[self.idx+2]
-                if not (pos1 if modes[0] == 1 else self.program[pos1]):
-                    self.idx = (pos2 if modes[1] == 1 else self.program[pos2])
-                else:
-                    self.idx += 3
-            elif opcode == 7:
-                pos1, pos2, pos3 = self.program[self.idx+1], self.program[self.idx+2], self.program[self.idx+3]
-                if (pos1 if modes[0] == 1 else self.program[pos1]) < (pos2 if modes[1] == 1 else self.program[pos2]):
-                    self.program[pos3] = 1
-                else:
-                    self.program[pos3] = 0
-                self.idx += 4
-            elif opcode == 8:
-                pos1, pos2, pos3 = self.program[self.idx+1], self.program[self.idx+2], self.program[self.idx+3]
-                if (pos1 if modes[0] == 1 else self.program[pos1]) == (pos2 if modes[1] == 1 else self.program[pos2]):
-                    self.program[pos3] = 1
-                else:
-                    self.program[pos3] = 0
-                self.idx += 4
-            else:
-                assert opcode == 99
-                self.state = 0
-                self.get_output()
-                break
+print(R)
